c40ad0fa805662c77ee26b44db0cd0b82bdef5e6
Waren Gador <waren.gador15@gmail.com>
2023-11-10 09:07:33 +0800

    update

--- src/index.jsx
+++ src/index.jsx
@@ -2,13 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './assets/sass/index.scss'
 import 'boxicons'
-import { BrowserRouter, RouterProvider } from 'react-router-dom'
+import { RouterProvider } from 'react-router-dom'
 import { router } from './components/router'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
       <RouterProvider router={router} />
-    </BrowserRouter>
   </React.StrictMode>,
 )
